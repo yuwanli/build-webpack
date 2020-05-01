d0b3c13824705fbda11ccf2857096e5af2d50e4e
wanli.yu <wanli.yu@beibei.com>
2020-05-01 16:46:28 +0800

    init

---
+++ test/demo/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="example">{{ msg }}</div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: 'Hello world! this is demo page'
+    }
+  }
+}
+</script>
+
+<style>
+.example {
+  color: yellowgreen;
+}
+</style>
---
+++ test/index/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="example">
+      <p>
+          {{ msg }}
+      </p>
+      <p class="test">{{msg}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: 'Hello world! this is index page'
+    }
+  },
+  created() {
+      console.log('index created')
+  }
+}
+</script>
+
+<style>
+.example {
+  display: flex;
+  color: red;
+}
+</style>
